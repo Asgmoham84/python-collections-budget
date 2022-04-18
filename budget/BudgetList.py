c5913d5c6ad7b20e1d0f7abac8c42f064d22b71d
Mohammadi, Asghar <amohammadi@cvent.com>
2022-04-18 18:52:44 -0400

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -24,12 +24,18 @@ class BudgetList():
 
 
 def main():
+    # Using above class
+    # Set starting budget to 500
     myBudgetList = BudgetList(1200)
+    # Add expenses, the last expense is 100 and that goes in overages
     expenses = Expense.Expenses()
-    expenses = expenses.read_expenses("data/spending_data.csv")
+    expenses.read_expenses('data/spending_data.csv')
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
-    print('The count of all expenses: '.format(str(len(myBudgetList))))
+
+    # Test len()
+    print('The count of all expenses: ' + str(len(myBudgetList)))
+
 
 if __name__ == "__main__":
     main()
